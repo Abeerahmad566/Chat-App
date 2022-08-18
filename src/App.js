37f89a1f450067a91e50fe04a382d61fda727056
Abeer Ahmed <abeerahmad389@gmail.com>
2022-08-18 18:21:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
 
-const socket = socketIO.connect("ws://localhost:4000");
+const socket = socketIO.connect("https://chat-server-socke.herokuapp.com/");
 
 function App() {
   return (
